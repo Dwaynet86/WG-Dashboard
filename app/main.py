626ac8f51e867925c9915f95e922dc15f68661d8
Dwayne Truex <51888733+Dwaynet86@users.noreply.github.com>
2025-10-19 21:50:25 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -178,7 +178,7 @@ async def add_client(request: Request,
     # call pivpn add
     if ip is None: # if no ip enetered use next available
         print(f"pivpn -a -n {client_name} -ip auto")
-       proc = subprocess.run(["pivpn", "-a", "-n", client_name, "-ip", "auto"], capture_output=True, text=True, timeout=10)
+        proc = subprocess.run(["pivpn", "-a", "-n", client_name, "-ip", "auto"], capture_output=True, text=True, timeout=10)
     else: # if ip enetered use it
        print(f"pivpn -a -n {client_name} -ip {ip}")
        proc = subprocess.run(["pivpn", "-a", "-n", client_name, "-ip", ip], capture_output=True, text=True, timeout=10) 
